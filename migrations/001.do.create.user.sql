f0e787a16f2903c9731de6f22fe6e2abf1d37755
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-08-05 14:43:01 -0400

    connection to db is working. only one table created at the moment. next create client side and try to save user info to db

--- migrations/001.do.create.user.sql
+++ migrations/001.do.create.user.sql
@@ -1,5 +1,5 @@
-CREATE TABLE 'user'(
-    'id' SERIAL PRIMARY KEY,
-    'full_name' VARCHAR NOT NULL,
-    'email' VARCHAR NOT NULL,
-)
+CREATE TABLE "user"(
+    "id" SERIAL PRIMARY KEY,
+    "full_name" VARCHAR NOT NULL,
+    "email" VARCHAR NOT NULL
+);
