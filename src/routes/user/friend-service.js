733aad9559cd65387b1dec4090e56fd6baada74f
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-08-20 17:40:10 -0400

    refactoring insert friend request service

--- src/routes/user/friend-service.js
+++ src/routes/user/friend-service.js
@@ -3,32 +3,11 @@ const FriendService = {
     //when sending friend request check if request already exists
    async insertFriendReq(db,request){
         console.log(request)
-             //searching if email exists
-        let value = await db.select('id').from('user').where('email',request.id)
-        let idRequest;
-        console.log('request value',value)
-        //if it does get id
-        if(value.length > 0){
-            idRequest = await value[0].id
-        } else {
-            console.log('no user by that email exists')
-        }
+             //searching through db to see if request already exists
+        let value = await db.select('id').from('friend').where('id',request.id)
+       
 
-        console.log('idRequest',idRequest)
-        if(request.id !== idRequest || request.id == null || request.id == undefined){
-            console.log('post friend request')
-            return db
-            .insert(request)
-            .into('friend')
-            .returning('*')
-            .then(([friend]) => friend)
-        } else {
-            console.log('friend request already exists')
-            return db
-            .select('*')
-            .from('friend')
-            .where('id',request.id)
-        }
+      
 
     },
     serializeRequset(req){
@@ -54,7 +33,7 @@ const FriendService = {
         if(!friendId){
             return {error: 'no friend id'}
         }
-        
+
         return db
             .select('id')
             .from('user')
