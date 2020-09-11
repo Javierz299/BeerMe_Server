e7f8818eadd8ea4cef0329f8c6d9bf6451c50052
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-09-11 13:04:14 -0400

    refac

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 
-    "postgres://zhmuaqrwefkroh:5e0f73de7cebce078454a97f9844661bd9c660e53830c904497932216c70e9dd@ec2-54-158-222-248.compute-1.amazonaws.com:5432/d1hjs1gv0g24m2",
+    "postgresql://postgres:Tucker15!@localhost/Beer_Me",
 }
