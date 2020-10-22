9820d27842150690c924d11a48729ea60f92f22f
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-10-22 11:57:43 -0400

    commit test

--- src/server.js
+++ src/server.js
@@ -18,20 +18,20 @@ app.use((error,req,res,next) => {
     res.status(500).json(response)
 })
 
-const db = knex({
-    client:  'pg',
-    connection: {
-        host: 'ec2-54-158-222-248.compute-1.amazonaws.com',
-        user: 'zhmuaqrwefkroh',
-        password: '5e0f73de7cebce078454a97f9844661bd9c660e53830c904497932216c70e9dd',
-        database: 'd1hjs1gv0g24m2'
-    }
-})
-
 // const db = knex({
 //     client:  'pg',
-//     connection: DATABASE_URL,
+//     connection: {
+//         host: 'ec2-54-158-222-248.compute-1.amazonaws.com',
+//         user: 'zhmuaqrwefkroh',
+//         password: '5e0f73de7cebce078454a97f9844661bd9c660e53830c904497932216c70e9dd',
+//         database: 'd1hjs1gv0g24m2'
+//     }
 // })
+
+const db = knex({
+    client:  'pg',
+    connection: DATABASE_URL,
+})
 
 app.set('db',db)
 
