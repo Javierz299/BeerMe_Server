d7d0efca0f74c769bee1a485899e80955dfb3ac7
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-10-22 10:39:04 -0400

    fetching drinks from drinktwo table

--- src/routes/user/graph-router/graph-service.js
+++ src/routes/user/graph-router/graph-service.js
@@ -3,9 +3,14 @@ const GraphService = {
         const allUserData = await db
             .select('user.id','user.username','drink.beer','drink.wine',
                     'drink.shots','drink.cocktail','seltzer',
-                    'drink.craft_beer')
+                    'drink.craft_beer','drinkTwo.duce','drinkTwo.eight_n_up',
+                    'drinkTwo.beer_bong','drinkTwo.shotgun','drinkTwo.wine_flight',
+                    'drinkTwo.beer_flight')
             .from('user')
             .innerJoin('drink','drink.user_id','user.id')
+            .innerJoin('drinkTwo','drinkTwo.user_id','user.id')
+
+        console.log('alluserdata',allUserData)
 
         return allUserData
     },
